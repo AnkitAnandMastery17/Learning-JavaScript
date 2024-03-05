86c5fc8c74f33c097eb50bc414eacf2728cb86ac
Ankit Anand <1930007@kiit.ac.in>
2024-03-05 06:15:55 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 15554
+var accountPassword = "5681321"
+let accountEmail = "abd@gmail.com"
+accountCity = "Patna"
+
+accountEmail = "ght@gmail.com"
+accountPassword = "67676"
+accountCity = "Delhi"
+// accountId = 4 [ not allowed]
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
